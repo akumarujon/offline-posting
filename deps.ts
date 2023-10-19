472d5eed45570117993069df9536b4a99b529065
akumaru <isakulovdev@gmail.com>
2023-10-19 22:19:39 +0500

    manage `deps`

--- deps.ts
+++ deps.ts
@@ -1,2 +1,4 @@
 export * from "https://deno.land/x/grammy@v1.18.1/mod.ts";
 export { serve } from "https://deno.land/std@0.163.0/http/server.ts";
+export { Application, Router, Context } from "https://deno.land/x/oak@v12.6.1/mod.ts";
+import "https://deno.land/std@0.204.0/dotenv/load.ts";
